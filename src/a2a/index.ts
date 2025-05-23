397178e6cef414b898845fa1d15ce9fc656e2f15
Georges Chitiga <cgeorges89@gmail.com>
2025-05-23 11:12:18 +0200

    fix exports of a2a types and classes

--- src/a2a/index.ts
+++ src/a2a/index.ts
@@ -1,4 +1,9 @@
 export * from "./common/A2AProtocol";
 export * from "./common/types";
 export { A2AServer, defaultAgentToTaskHandlerAdapter } from "./server";
+export type {
+  A2AServerOptions,
+  A2ATaskHandler,
+  AgentToTaskHandlerAdapter,
+} from "./server";
 export * from "./client"; // This will export from src/a2a/client/index.ts
